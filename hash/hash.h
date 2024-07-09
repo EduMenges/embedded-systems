ff755f828a35211ef67736e27840482793570df2
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-07-08 21:57:01 -0300

    Added benchmark for hash

--- hash/hash.h
+++ hash/hash.h
@@ -1,4 +1,11 @@
 #include <cstdint>
 #include <cstddef>
 
-uint32_t hash(const uint8_t *str, size_t len, uint32_t seed);
+/**
+ * @brief Hash using funny-falcon's GoodOAAT hash (https://github.com/rurban/smhasher/blob/master/Hashes.cpp)
+ * @param data Pointer to data
+ * @param len Data length **in_bytes**
+ * @param seed Hash's seed
+ * @return uint32_t The hash of data
+ */
+uint32_t hash(const void *data, size_t len, uint32_t seed = 0);
