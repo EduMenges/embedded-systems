55d3b5ae63963b8683b1aa6aab46d6b6913d3426
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-07-14 19:35:19 -0300

    Added new images

--- laplacian/laplacian.h
+++ laplacian/laplacian.h
@@ -1,9 +1,8 @@
 #ifndef IMAGE_ARRAY_H
 #define IMAGE_ARRAY_H
 
-#define IMAGE_WIDTH 273
-#define IMAGE_HEIGHT 275
+#include "1024x1024_image.h"
 
 void applyLaplacian(unsigned char src[IMAGE_HEIGHT][IMAGE_WIDTH], int dst[IMAGE_HEIGHT][IMAGE_WIDTH]);
 
-#endif // IMAGE_ARRAY_H
+#endif
